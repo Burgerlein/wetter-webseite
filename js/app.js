e41a9d22fb3be6763041f0af49734b1f94ee3cdf
Tim <90694125+Burgerlein@users.noreply.github.com>
2022-06-17 09:35:14 +0000

    Update

--- js/app.js
+++ js/app.js
@@ -22,3 +22,28 @@ function validate() {
         alert('Flasche Eingbae')  
       }
 }
+
+/* Side Meneu*/
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+/*Drop Down in Side Meneu*/
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+  
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
